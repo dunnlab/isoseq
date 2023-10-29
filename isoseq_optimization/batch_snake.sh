01edd9ce1e2f9f3af0c7c7d8ba273277ac26f820
Casey Dunn <casey.dunn@yale.edu>
2023-10-29 12:36:37 -0400

    added batch

---
+++ isoseq_optimization/batch_snake.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=snake
+#SBATCH --output=snake.txt
+#SBATCH --time=3-00:00:00
+#SBATCH --partition=ycga_long
+#SBATCH --nodes=1                    # number of cores and nodes
+#SBATCH --cpus-per-task=16           # number of cores
+#SBATCH --mem-per-cpu=10G            # shared memory, scaling with CPU request
+
+module load miniconda
+snakemake --cores $SLURM_CPUS_PER_TASK
