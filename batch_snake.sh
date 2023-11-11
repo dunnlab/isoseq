8729e98c1e49d96b8668413965e06d3caf4897b3
Casey Dunn <casey.dunn@yale.edu>
2023-11-10 23:03:52 -0500

    use nr

--- batch_snake.sh
+++ batch_snake.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=ycga
 #SBATCH --nodes=1                    # number of cores and nodes
-#SBATCH --cpus-per-task=16           # number of cores
+#SBATCH --cpus-per-task=32           # number of cores
 #SBATCH --mem-per-cpu=5G            # shared memory, scaling with CPU request
 
 module load miniconda
